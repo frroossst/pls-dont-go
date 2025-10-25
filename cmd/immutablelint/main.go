1e106db73c4680bb42cf90253d838cacd3c66852
Adhyan H <adhyanpatel@gmail.com>
2025-10-24 22:24:31 -0600

    added versions

--- cmd/immutablelint/main.go
+++ cmd/immutablelint/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"strings"
+
 	"github.com/frroossst/pls-dont-go/immutablecheck"
 
 	"golang.org/x/tools/go/analysis/singlechecker"
 )
 
 func main() {
+	// quick-and-dirty: if -V or -V=* seen, print version and exit
+	for _, a := range os.Args[1:] {
+		if a == "-V" || strings.HasPrefix(a, "-V=") || a == "--version" {
+			fmt.Println("adhyan-dev-v0.3.0")
+		}
+	}
+
 	singlechecker.Main(immutablecheck.Analyzer)
 }
